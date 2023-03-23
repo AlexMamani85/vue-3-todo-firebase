5fb7061afcdb3239db3f38ede1a1de7fe9d3a245
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2023-03-23 00:31:04 -0500

    CHORE: 123. Fetching All Collection Documents

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@
 </template>
 
 <script setup>
-import { prepareProjectsData, fetchSingleDocument } from './firebase/project'
+import { prepareProjectsData, fetchSingleDocument, fetchAllDocuments } from './firebase/project'
 import BaseCheckbox from "./components/base/BaseCheckbox.vue";
 import AddTaskInput from "./components/task/AddTaskInput.vue";
 import TodoListItem from "./components/task/TodoListItem.vue";
@@ -50,6 +50,7 @@ import { computed } from "vue";
 let nextTaskId = 100;
 await prepareProjectsData();
 await fetchSingleDocument();
+await fetchAllDocuments();
 const store = useStore();
 const activeProjectId = computed(() => store.state.project.activeProjectId);
 const projects = computed(() => store.getters[`project/projectsWithStats`]);
