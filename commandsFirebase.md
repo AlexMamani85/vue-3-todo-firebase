5c028230db3bc2374737d0af48b9c6779627b073
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2023-03-22 22:28:52 -0500

    CHORE: 121. Setting Up Data First (Important!)

--- commandsFirebase.md
+++ commandsFirebase.md
@@ -34,3 +34,22 @@ src/firebase/firebase.js
 (paste)
 export const app = initializeApp(firebaseConfig);
 ```
+
+# 119. Playing with Firestore
+
+```
+Start Collection
+CollectionID: projects
+(Document parent path: projects)
+Document ID: Auto-ID
+Field, Type, Value: name, string, First project
+Save
+```
+
+```
+Delete Collection NO
+```
+
+# 121. Setting Up Data First (Important)
+
+npm install firebase
--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,7 @@
 </template>
 
 <script setup>
+import { prepareProjectsData } from './firebase/project'
 import BaseCheckbox from "./components/base/BaseCheckbox.vue";
 import AddTaskInput from "./components/task/AddTaskInput.vue";
 import TodoListItem from "./components/task/TodoListItem.vue";
@@ -47,6 +48,7 @@ import {
 import { useStore } from "vuex";
 import { computed } from "vue";
 let nextTaskId = 100;
+await prepareProjectsData();
 const store = useStore();
 const activeProjectId = computed(() => store.state.project.activeProjectId);
 const projects = computed(() => store.getters[`project/projectsWithStats`]);
